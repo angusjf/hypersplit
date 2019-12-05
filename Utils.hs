88a2570d4cd0899dda4a880a0edb214950d3f1f7
angusjf <el17ajf@leeds.ac.uk>
2019-12-05 09:05:08 +0100

    added pixel sorting

--- Utils.hs
+++ Utils.hs
@@ -1,12 +1,16 @@
 module Utils where
 
 import System.Random
+import Data.List
 import qualified Graphics.Image as I
 
 data ColorComponent = Red | Blue | Green
 
 type Picture = I.Image I.VU I.RGB Double
 type Picxel = I.Pixel I.RGB Double
+
+brightness :: Picxel -> Double
+brightness (I.PixelRGB r g b) = r + g + b
 
 rgb :: Picture -> (Picture, Picture, Picture)
 rgb img = (red, green, blue)
@@ -33,11 +37,26 @@ infinisplit :: StdGen -> [StdGen]
 infinisplit g = g1:infinisplit g2
   where (g1, g2) = split g
 
-applyFnsToRows :: [[a] -> [b]] -> [[a]] -> [[b]]
-applyFnsToRows rowFns rows = zipWith (\rf row -> rf row) rowFns rows
-
 repeatElems :: [Int] -> [a] -> [a]
 repeatElems (n:ns) all@(x:_) = repeats ++ repeatElems ns rest
   where repeats = take (length all) $ replicate n x
         rest    = drop n $ all
 repeatElems _ xs = xs
+
+toBlack :: Picture -> Picture
+toBlack = I.map (const black)
+  where black :: Picxel
+        black = I.PixelRGB 0 0 0
+
+applyRowFnsToImage :: [[Picxel] -> [Picxel]] -> Picture -> Picture
+applyRowFnsToImage fns = I.fromLists . zipWith ($) fns . I.toLists
+
+roughSortBy :: StdGen -> (Int, Int) -> (a -> a -> Ordering) -> [a] -> [a]
+roughSortBy _ _ _ [] = []
+roughSortBy g range fn list = sortBy fn start ++ roughSortBy g' range fn end
+  where (n, g') = randomR range g
+        start = take n list
+        end   = drop n list
+
+verticalize :: (Picture -> Picture) -> Picture -> Picture
+verticalize fn = I.transpose . fn . I.transpose
--- hyperSplit.hs
+++ hyperSplit.hs
@@ -3,6 +3,7 @@ module HyperSplit where
 import qualified Graphics.Image as I
 import System.Environment
 import System.Random
+import Data.List
 import Utils
 
 run :: (StdGen -> Picture -> Picture) -> String -> IO ()
@@ -11,28 +12,26 @@ run fn infile = do
         g <- getStdGen
         I.displayImage $ fn g img
 
-horizontalBleedRgb :: StdGen -> Picture -> Picture
-horizontalBleedRgb g = applyRgb (bleed g1, bleed g2, bleed g3)
-  where (g1, g2, g3) = split3 g
-
-verticalBleedRgb :: StdGen -> Picture -> Picture
-verticalBleedRgb g = I.transpose . horizontalBleedRgb g . I.transpose
-
-verticalPixelSort :: StdGen -> Picture -> Picture
-verticalPixelSort _ = I.transpose . id . I.transpose
-
---hyperSplit :: StdGen -> Picture -> Picture
---hyperSplit g = map (drop Green . (applyRgb (bleed g1, toBlack, bleed g3)))
---  where (g1, g2, g3) = split3 g
-
-applyRowFnsToImage :: [[Picxel] -> [Picxel]] -> Picture -> Picture
-applyRowFnsToImage fns = I.fromLists . applyFnsToRows fns . I.toLists
-
 bleed :: StdGen -> Picture -> Picture
 bleed g = applyRowFnsToImage (map repeatElems rands2d)
   where rands2d = map (randomRs (5, 15)) (infinisplit g)
 
-toBlack :: Picture -> Picture
-toBlack = I.map (const black)
-  where black :: Picxel
-        black = I.PixelRGB 0 0 0
+horizontalBleedRgb :: StdGen -> Picture -> Picture
+horizontalBleedRgb g = applyRgb (bleed g1, bleed g2, bleed g3)
+  where (g1, g2, g3) = split3 g
+
+horizontalPixelSort :: StdGen -> Picture -> Picture
+horizontalPixelSort g = applyRowFnsToImage rowFns
+  where rowFns = map (\gen -> roughSortBy gen (4, 36) sortFn) (infinisplit g)
+        sortFn = \a b -> compare (brightness a) (brightness b)
+
+horizontalPixelSortRgb :: StdGen -> Picture -> Picture
+horizontalPixelSortRgb g = applyRgb (horizontalPixelSort g, id, id)
+
+verticalBleedRgb = verticalize . horizontalBleedRgb
+verticalPixelSort = verticalize . horizontalPixelSort
+verticalPixelSortRgb = verticalize . horizontalPixelSortRgb
+
+--hyperSplit :: StdGen -> Picture -> Picture
+--hyperSplit g = map (drop Green . (applyRgb (bleed g1, toBlack, bleed g3)))
+--  where (g1, g2, g3) = split3 g
